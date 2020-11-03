4788bf523ef8e742334ef484370d61a7c90246cb
Uncle Engineer <loong.wissawakorn@gmail.com>
2020-11-04 00:27:18 +0700

    First Upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main(){
+	printf(" \n");
+	printf(" \n");
+	printf(" \n");
+	printf(" \n");
+	printf(" ------------------ \n");
+	printf(" \n");
+	printf("    HELLO WORLD\n");
+	printf(" \n");
+	printf(" \n");
+	printf(" by UNCLE ENGINEER\n");
+	printf(" \n");
+	printf(" ------------------ \n");
+}
